75ba11a541a0112515e9be1deae434f8f96b6964
Your NameAlex <alehr45@gmail.com>
2021-01-27 14:43:14 -0600

    adding 5 day forecast api

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,30 +1,71 @@
+var momentEl = moment().format('MMMM Do YYYY, h:mm:ss a');
+var today = document.querySelector("#currentDay");
 
-
+today.innerHTML = (momentEl);
 
 saveCity = function () {
-  var citySearchElement = document.getElementById("userinput1");
-  var citySearch = "Nashville"
+
+
+  // var citySearchElement = document.getElementById("userinput1");
+  var citySearch = "jacksonville"
   var apiKey = "5eb37a19973c9457201128f6d1d5ae80";
   var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + apiKey;
+
+
 
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
         console.log(response);
         response.json().then(function (data) {
-          getWeather(data);
+          console.log(data);
         });
       }
     });
 
 };
 
-function getWeather( e ) {
-	var fahrenheit = Math.round(((parseFloat(e.main.temp)-273.15)*1.8)+32); 
-	document.getElementById('description').innerHTML = e.weather[0].description;
-	document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
-	document.getElementById('location').innerHTML = e.name;
+getForecast = function () {
+  var citySearch = "jacksonville"
+  var apiKey = "5eb37a19973c9457201128f6d1d5ae80";
+  var apiUrl2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=" + apiKey;
+  
+
+  fetch(apiUrl2)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          getForecast1(data);
+        });
+      }
+    });
+
+    
+
 }
 
+function getWeather(data) {
+  // var icon = data.
 
+  var fahrenheit = Math.round(((parseFloat(data.main.temp) - 273.15) * 1.8) + 32);
+  document.getElementById('location').innerHTML = data.name;
+  document.getElementById('description').innerHTML = data.weather[0].description;
+  document.getElementById('temp').innerHTML = fahrenheit + '&deg;' + " F";
+  document.getElementById('humidity').innerHTML = "Humidity: " + data.main.humidity + "%";
+  document.getElementById('windspeed').innerHTML = "Wind Speed: " + data.wind.speed + "mph";
+  document.getElementById('windspeed').innerHTML = "Wind Speed: " + data.wind.speed + "mph";
+  
+ };
+  
+ 
+ function getForecast1(data) {
+  document.getElementById('tomorrow').innerHTML = Math.round(((parseFloat(data.list[5].main.temp) - 273.15) * 1.8) + 32);
+
+  };
+
+
+
+
+getForecast();
 saveCity();
