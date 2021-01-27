6990b9aa753bf61145d5c43d15c2ef9147cab56e
Your NameAlex <alehr45@gmail.com>
2021-01-27 16:35:28 -0600

    adding html for 5 day forecast

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -7,7 +7,7 @@ saveCity = function () {
 
 
   // var citySearchElement = document.getElementById("userinput1");
-  var citySearch = "jacksonville"
+  var citySearch = "los angeles"
   var apiKey = "5eb37a19973c9457201128f6d1d5ae80";
   var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + apiKey;
 
@@ -18,7 +18,7 @@ saveCity = function () {
       if (response.ok) {
         console.log(response);
         response.json().then(function (data) {
-          console.log(data);
+          getWeather(data);
         });
       }
     });
@@ -26,10 +26,10 @@ saveCity = function () {
 };
 
 getForecast = function () {
-  var citySearch = "jacksonville"
+  var citySearch = "los angeles"
   var apiKey = "5eb37a19973c9457201128f6d1d5ae80";
   var apiUrl2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=" + apiKey;
-  
+
 
   fetch(apiUrl2)
     .then(function (response) {
@@ -41,28 +41,35 @@ getForecast = function () {
       }
     });
 
-    
+
 
 }
 
 function getWeather(data) {
-  // var icon = data.
-
+  var icon = document.querySelector(".icon").innerHTML = "<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png' alt='Icon depicting current weather.'>";
   var fahrenheit = Math.round(((parseFloat(data.main.temp) - 273.15) * 1.8) + 32);
+  document.getElementById("icon").innerHTML = icon
   document.getElementById('location').innerHTML = data.name;
-  document.getElementById('description').innerHTML = data.weather[0].description;
   document.getElementById('temp').innerHTML = fahrenheit + '&deg;' + " F";
   document.getElementById('humidity').innerHTML = "Humidity: " + data.main.humidity + "%";
   document.getElementById('windspeed').innerHTML = "Wind Speed: " + data.wind.speed + "mph";
   document.getElementById('windspeed').innerHTML = "Wind Speed: " + data.wind.speed + "mph";
-  
- };
-  
- 
- function getForecast1(data) {
-  document.getElementById('tomorrow').innerHTML = Math.round(((parseFloat(data.list[5].main.temp) - 273.15) * 1.8) + 32);
 
-  };
+
+};
+
+
+function getForecast1(data) {
+  document.querySelector(".onedayicon").innerHTML = "<img src='http://openweathermap.org/img/w/" + data.list[4].weather[0].icon + ".png' alt='Icon depicting current weather.'>";
+  document.getElementById('onedaydate').innerHTML = moment().add(1, 'days').format('dddd');
+  document.getElementById('onedaytemp').innerHTML = "Temp: " + Math.round(((parseFloat(data.list[4].main.temp) - 273.15) * 1.8) + 32) + '&deg;';
+  document.getElementById('onedayhumidity').innerHTML = "Humidity: " + data.list[5].main.humidity + "%";
+  document.querySelector(".twodayicon").innerHTML = "<img src='http://openweathermap.org/img/w/" + data.list[12].weather[0].icon + ".png' alt='Icon depicting current weather.'>";
+  document.getElementById('twodaydate').innerHTML = moment().add(2, 'days').format('dddd');
+  document.getElementById('twodaytemp').innerHTML = "Temp: " + Math.round(((parseFloat(data.list[12].main.temp) - 273.15) * 1.8) + 32) + '&deg;';
+  document.getElementById('twodayhumidity').innerHTML = "Humidity: " + data.list[12].main.humidity + "%";
+
+};
 
 
 
