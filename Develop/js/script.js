2413030172133223e9863186a16c24f122611fda
Your NameAlex <alehr45@gmail.com>
2021-01-27 12:15:31 -0600

    certain weather data shows on screen

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,13 +1,30 @@
-var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=5eb37a19973c9457201128f6d1d5ae80" 
-var citySearch = document.querySelector("userinput");
 
 
-fetch(apiUrl)
-    .then(function(response) {
+
+saveCity = function () {
+  var citySearchElement = document.getElementById("userinput1");
+  var citySearch = "Nashville"
+  var apiKey = "5eb37a19973c9457201128f6d1d5ae80";
+  var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + apiKey;
+
+  fetch(apiUrl)
+    .then(function (response) {
       if (response.ok) {
         console.log(response);
-        response.json().then(function(data) {
-          console.log(data);
+        response.json().then(function (data) {
+          getWeather(data);
         });
+      }
+    });
 
-    
+};
+
+function getWeather( e ) {
+	var fahrenheit = Math.round(((parseFloat(e.main.temp)-273.15)*1.8)+32); 
+	document.getElementById('description').innerHTML = e.weather[0].description;
+	document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
+	document.getElementById('location').innerHTML = e.name;
+}
+
+
+saveCity();
